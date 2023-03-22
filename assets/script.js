79c233a010ecca2801c7107b8db47fa475a81181
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-22 17:27:17 +0100

    [Test] Creazione griglia senza condizioni

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
+// Verifico il corretto funzionamento dello script 
 console.log(document)
+
+//Intercetto il container padre e lo inizializzo in una var
+const container = document.querySelector(".container");
+console.log(container)
+
+//Creo il ciclo for da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  console.log('numero che si incrementa da 1 a 100 -->', i)
+
+  //Ad ogni ciclo creo un elemento DIV senza classe
+  const square = document.createElement("div");
+  console.log(square);
+
+  //Ora assegno una classe preimpostata da css
+  square.classList.add("square");
+  console.log(square.classList);
+
+  //Provo ad inserirla nel Dom
+  container.appendChild(square);
+}
